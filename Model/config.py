bb3681333a45efb5fd9a656e3ef9e8a544dac161
Rusano <104228162+khann160102@users.noreply.github.com>
2022-06-24 10:51:08 +0700

    Add files via upload

---
+++ Model/config.py
@@ -0,0 +1,10 @@
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+k = 5
+step = 5
+features = 512
+
+path = 'D:\\Documents\\5. Summer 2022\\'
+input = path + 'features\\'
+output = path + 'label_' + str(k) + '\\'
